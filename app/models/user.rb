92a2a3edd55f1c011370cda78ea95939dc23467d
sakko <saklism@gmail.com>
2020-04-12 17:10:24 +0700

    user login done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  auth_token             :string
 #  confirmation_sent_at   :datetime
 #  confirmation_token     :string
 #  confirmed_at           :datetime
@@ -27,6 +28,7 @@
 #
 # Indexes
 #
+#  index_users_on_auth_token            (auth_token)
 #  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
@@ -36,6 +38,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  before_validation :generate_auth_token, on: [:create]
+
+  def generate_auth_token(force = false)
+    if self.new_record?
+      self.auth_token ||= SecureRandom.uuid
+    else
+      if self.auth_token.blank? || force
+        self.update_columns(auth_token: SecureRandom.uuid)
+      end
+    end
+  end
 
   def sign_in_json
     json = {
@@ -49,6 +62,6 @@ class User < ApplicationRecord
   end
 
   def auth_jwt(duration = 15.days.from_now.to_i)
-    ApiJwt.encrypt_jwt({ id: self.id }, Rails.application.credentials.user_jwt_secret, duration)
+    ApiJwt.encrypt_jwt({ auth_token: self.auth_token }, Rails.application.credentials.user_jwt_secret, duration)
   end
 end
